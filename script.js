b8563c511d39fa7dfcea903bf8899a57f1e17ddd
player <reza.linusx@gmail.com>
2025-07-17 23:15:11 +0330

    Support uploading and displaying multiple images in seperate canvases

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const addImageButton = document.getElementById("add-image");
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const container = document.querySelector(".container");
 
 addImageButton.addEventListener("change", (e) => {
   const file = e.target.files[0];
@@ -8,10 +7,22 @@ addImageButton.addEventListener("change", (e) => {
     const img = new Image();
     img.src = URL.createObjectURL(file);
     img.onload = () => {
-      canvas.height = img.naturalHeight;
-      canvas.width = img.naturalWidth;
-      context.drawImage(img, 0, 0);
+      const myCanvas = createCanvas();
+      drawImageOnCanvas(myCanvas, img);
+      container.appendChild(myCanvas);
     };
   }
 });
 
+function createCanvas() {
+  const canvas = document.createElement("canvas");
+  canvas.className = "vas";
+  return canvas;
+}
+
+function drawImageOnCanvas(myCanvas, img) {
+  const context = myCanvas.getContext("2d");
+  myCanvas.height = img.naturalHeight;
+  myCanvas.width = img.naturalWidth;
+  context.drawImage(img, 0, 0);
+}
